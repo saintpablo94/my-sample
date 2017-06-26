3406b8c1b8d76b607b839ae269756eb453d2a73c
saintpablo94 <saint.pablo94@gmail.com>
2017-06-26 12:12:46 +0900

    initial commit

---
+++ src/main/java/net/sample/MySampleApplication.java
@@ -0,0 +1,12 @@
+package net.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySampleApplication.class, args);
+	}
+}
