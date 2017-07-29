ede44c571804b6c58852cd405d5ac81288b70635
saintpablo94 <saint.pablo94@gmail.com>
2017-07-29 09:16:23 +0900

    [sus]질문

--- src/main/java/net/sample/domain/User.java
+++ src/main/java/net/sample/domain/User.java
@@ -23,6 +23,10 @@ public class User {
 		this.userId = userId;
 	}
 
+	public String getUserId() {
+		return userId;
+	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
