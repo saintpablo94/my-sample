b815d3abca081e8b27adb3c3d534bcb7a03669c9
saintpablo94 <saint.pablo94@gmail.com>
2017-07-30 17:44:07 +0900

    [sus]war

--- src/main/java/net/sample/MyWebInitializer.java
+++ src/main/java/net/sample/MyWebInitializer.java
@@ -1,5 +1,11 @@
 package net.sample;
 
-public class MyWebInitializer {
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+public class MyWebInitializer extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MySampleApplication.class);
+	}
 }
