e78db0078f28830330600b76900c71d8f8b56240
saintpablo94 <saint.pablo94@gmail.com>
2017-07-30 16:04:09 +0900

    [sus] question list

--- src/main/java/net/sample/web/HomeController.java
+++ src/main/java/net/sample/web/HomeController.java
@@ -2,12 +2,22 @@ package net.sample.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
+import net.sample.domain.QuestionRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private QuestionRepository questionRepository;
+	
+	
 	@GetMapping("")
-	public String home(){
+	public String home(Model model){
+		model.addAttribute("questions", questionRepository.findAll());
 		return "index";
 	}
 }
