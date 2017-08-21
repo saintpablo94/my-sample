ad822ba895c30c1b588865b9a411fe026e132dd6
saintpablo94 <saint.pablo94@gmail.com>
2017-08-21 19:32:37 +0900

    question

--- src/main/java/net/sample/domain/Question.java
+++ src/main/java/net/sample/domain/Question.java
@@ -1,8 +1,14 @@
 package net.sample.domain;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Question {
@@ -11,18 +17,40 @@ public class Question {
 	@GeneratedValue
 	private Long id;
 
-	private String writer;
+	@ManyToOne
+	@JoinColumn(foreignKey = @ForeignKey(name = "fk_question_writer"))
+	private User writer;
+
 	private String title;
 	private String contents;
+
+	private LocalDateTime createDate;
 
 	public Question() {
 	}
 
-	public Question(String writer, String title, String contents) {
+	public Question(User writer, String title, String contents) {
 		super();
 		this.writer = writer;
 		this.title = title;
 		this.contents = contents;
+		this.createDate = LocalDateTime.now();
 	}
 
+	public String getFormattedCreateDate() {
+		if (createDate == null) {
+			return "";
+		}
+		return createDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
+	}
+
+	public void update(String title, String contents) {
+		this.title = title;
+		this.contents = contents;
+	}
+
+	public boolean isSameWriter(User loginUser) {
+
+		return this.writer.equals(loginUser);
+	}
 }
--- src/main/java/net/sample/web/QuestionController.java
+++ src/main/java/net/sample/web/QuestionController.java
@@ -1,11 +1,15 @@
 package net.sample.web;
 
 import javax.servlet.http.HttpSession;
-
+import org.hibernate.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.sample.domain.Question;
@@ -26,7 +30,7 @@ public class QuestionController {
 			return "/users/loginForm";
 		}
 
-		return "qna/form";
+		return "/qna/form";
 	}
 
 	@PostMapping("")
@@ -37,9 +41,66 @@ public class QuestionController {
 		}
 
 		User sessionUser = HttpSessionUtils.getUserFromSession(session);
-		Question newQuestion = new Question(sessionUser.getUserId(), title, contents);
+		Question newQuestion = new Question(sessionUser, title, contents);
 		questionRepository.save(newQuestion);
 
+		return "redirect:/";
+	}
+
+	@GetMapping("/{id}")
+	public String show(@PathVariable Long id, Model model) {
+		model.addAttribute("question", questionRepository.findOne(id));
+		return "/qna/show";
+	}
+
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model, HttpSession session){
+		if (!HttpSessionUtils.isLoginUser(session)) {
+			return "/users/loginForm";
+		}
+		
+		User loginUser = HttpSessionUtils.getUserFromSession(session);
+		Question question = questionRepository.findOne(id);
+		if(!question.isSameWriter(loginUser)){
+			return "/users/loginForm";
+		}
+		
+		model.addAttribute("question",question);
+		return "/qna/updateForm";
+	}
+
+	@PutMapping("/{id}")
+	public String update(@PathVariable Long id, String title, String contents, HttpSession session) {
+		
+		if (!HttpSessionUtils.isLoginUser(session)) {
+			return "/users/loginForm";
+		}
+		
+		User loginUser = HttpSessionUtils.getUserFromSession(session);
+		Question question = questionRepository.findOne(id);
+		if(question.isSameWriter(loginUser)){
+			return "/users/loginForm";
+		}
+		
+		question.update(title, contents);
+		questionRepository.save(question);
+		return String.format("redirect:/questions/%d", id);
+	}
+
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable Long id, HttpSession session) {
+		
+		if (!HttpSessionUtils.isLoginUser(session)) {
+			return "/users/loginForm";
+		}
+		
+		User loginUser = HttpSessionUtils.getUserFromSession(session);
+		Question question = questionRepository.findOne(id);
+		if(question.isSameWriter(loginUser)){
+			return "/users/loginForm";
+		}
+		
+		questionRepository.delete(id);
 		return "redirect:/";
 	}
 }
